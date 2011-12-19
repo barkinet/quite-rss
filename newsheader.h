7e3d511c78cd30dbf732194ef555d8f166f34545
Aleksey Khokhryakov <hohryakov@simos.ru>
2011-12-19 15:47:32 +0500

    added: ????????? ???? "?????" ? ???????? (issue: 00092)

---
+++ newsheader.h
@@ -0,0 +1,38 @@
+#ifndef NEWSHEADER_H
+#define NEWSHEADER_H
+
+#include <QtGui>
+#include "newsmodel.h"
+
+class NewsHeader : public QHeaderView
+{
+  Q_OBJECT
+public:
+  NewsHeader(Qt::Orientation orientation, QWidget * parent = 0);
+  NewsModel *model_;
+  void initColumns();
+  void createMenu();
+  void overload();
+
+public slots:
+
+protected:
+  bool eventFilter(QObject *, QEvent *);
+  virtual void mousePressEvent(QMouseEvent*);
+  virtual void mouseMoveEvent(QMouseEvent*);
+
+private slots:
+  void slotButtonColumnView();
+  void columnVisible(QAction*);
+
+signals:
+
+private:
+  QMenu *viewMenu_;
+  QPushButton *buttonColumnView;
+  int idxCol;
+  int posX;
+
+};
+
+#endif // NEWSHEADER_H
