ae46f2c911393deaecd809996fff344656f25b4d
Egor <Egor@localhost>
2011-12-22 11:48:59 +0600

    added: ?? ????????? ?????????? ???????? ?? ????. refactored: ?????? ????????? ????.

---
+++ feedsmodel.cpp
@@ -0,0 +1,56 @@
+#include <QDebug>
+#include <QFont>
+#include <QBrush>
+#include <QIcon>
+
+#include "feedsmodel.h"
+
+FeedsModel::FeedsModel(QObject *parent)
+  :QSqlTableModel(parent)
+{
+}
+
+QVariant FeedsModel::data(const QModelIndex &index, int role) const
+{
+  if (role == Qt::FontRole) {
+    QFont font;
+    if ((0 < QSqlTableModel::index(index.row(), fieldIndex("unread")).data(Qt::EditRole).toInt()) &&
+        (QSqlTableModel::fieldIndex("unread") != index.column()))
+      font.setBold(true);
+    return font;
+  } else if (role == Qt::DisplayRole){
+    if (QSqlTableModel::fieldIndex("unread") == index.column()) {
+      if (0 == QSqlTableModel::index(index.row(), fieldIndex("unread")).data(Qt::EditRole).toInt()) {
+        return QVariant();
+      } else {
+        QString qStr = QString("(%1)").
+            arg(QSqlTableModel::index(index.row(), fieldIndex("unread")).data(Qt::EditRole).toInt());
+        return qStr;
+      }
+    }
+  } else if (role == Qt::TextColorRole) {
+    QBrush brush;
+    if (QSqlTableModel::fieldIndex("unread") == index.column()) {
+      brush.setColor("#0000CA");
+    }
+    return brush;
+  } else if (role == Qt::DecorationRole) {
+    if (QSqlTableModel::fieldIndex("text") == index.column()) {
+      QIcon icon;
+      icon.addFile(":/images/feed");
+      return icon;
+    }
+  } else if (role == Qt::TextAlignmentRole) {
+    if (QSqlTableModel::fieldIndex("id") == index.column()) {
+      int flag = Qt::AlignRight|Qt::AlignVCenter;
+      return flag;
+    }
+  }
+
+  return QSqlTableModel::data(index, role);
+}
+
+/*virtual*/ bool	FeedsModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+  return QSqlTableModel::setData(index, value, role);
+}
---
+++ newsmodel.cpp
@@ -0,0 +1,69 @@
+#include <QDebug>
+#include <QFont>
+#include <QIcon>
+#include <QDateTime>
+
+#include "newsmodel.h"
+
+NewsModel::NewsModel(QObject *parent)
+  :QSqlTableModel(parent)
+{
+}
+
+QVariant NewsModel::data(const QModelIndex &index, int role) const
+{ 
+  if (role == Qt::DecorationRole) {
+    if (QSqlTableModel::fieldIndex("read") == index.column()) {
+      QIcon icon;
+      if (0 == QSqlTableModel::index(index.row(), fieldIndex("read")).data(Qt::EditRole).toInt())
+        icon.addFile(":/images/bulletUnread");
+      else icon.addFile(":/images/bulletRead");
+      return icon;
+    } else if (QSqlTableModel::fieldIndex("sticky") == index.column()) {
+      QIcon icon;
+      if (0 == QSqlTableModel::index(index.row(), fieldIndex("sticky")).data(Qt::EditRole).toInt())
+        icon.addFile(":/images/starOff");
+      else icon.addFile(":/images/starOn");
+      return icon;
+    }
+  } else if (role == Qt::DisplayRole) {
+    if (QSqlTableModel::fieldIndex("read") == index.column()) {
+      return QVariant();
+    }
+    else if (QSqlTableModel::fieldIndex("sticky") == index.column()) {
+      return QVariant();
+    }
+    else if (QSqlTableModel::fieldIndex("published") == index.column()) {
+      QString strPublished;
+      strPublished = QSqlTableModel::index(index.row(), fieldIndex("published")).data(Qt::EditRole).toString();
+      QLocale locale(QLocale::C);
+      QDateTime dt = locale.toDateTime(strPublished, "yyyy-MM-ddTHH:mm:ss");
+      int daysToCurrentDT = dt.daysTo(QDateTime::currentDateTimeUtc());
+      if (daysToCurrentDT < 0) {
+        qWarning() << "News has future published Date";
+        return QString();
+      }
+      else if (daysToCurrentDT < 1) return dt.toString("hh:mm");
+      else return dt.toString("yyyy.MM.dd hh:mm");
+    }
+    else if (QSqlTableModel::fieldIndex("received") == index.column()) {
+      QDateTime dateTime = QDateTime::fromString(
+            QSqlTableModel::index(index.row(), fieldIndex("received")).data(Qt::EditRole).toString(),
+            Qt::ISODate);
+      if (QDateTime::currentDateTime().date() == dateTime.date()) {
+        return dateTime.toString("hh:mm");
+      } else return dateTime.toString("yyyy.MM.dd hh:mm");
+    }
+  } else if (role == Qt::FontRole) {
+    QFont font;
+    if (0 == QSqlTableModel::index(index.row(), fieldIndex("read")).data(Qt::EditRole).toInt())
+      font.setBold(true);
+    return font;
+  }
+  return QSqlTableModel::data(index, role);
+}
+
+/*virtual*/ bool	NewsModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+  return QSqlTableModel::setData(index, value, role);
+}
