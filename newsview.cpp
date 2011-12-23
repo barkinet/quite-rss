ce2b5c39bc0cfbd7ae1c42bf4ccdfc6653ac6a1e
Egor <Egor@localhost>
2011-12-23 14:05:56 +0600

    changed: ??????? ?????? "? ????????".

---
+++ newsview.cpp
@@ -0,0 +1,48 @@
+#include "newsview.h"
+
+NewsView::NewsView(QWidget * parent) :
+    QTreeView(parent)
+{
+  setObjectName("newsView_");
+  setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+  setEditTriggers(QAbstractItemView::NoEditTriggers);
+  setMinimumWidth(120);
+  setSortingEnabled(true);
+  setSelectionBehavior(QAbstractItemView::SelectRows);
+  setSelectionMode(QAbstractItemView::SingleSelection);
+//    setAlternatingRowColors(true);
+}
+
+/*virtual*/ void NewsView::mousePressEvent(QMouseEvent *event)
+{
+  QModelIndex index = indexAt(event->pos());
+  if (index.column() == model_->fieldIndex("sticky")) {
+    QModelIndex curIndex = currentIndex();
+    if (model_->index(index.row(), model_->fieldIndex("sticky")).data(Qt::EditRole).toInt() == 0) {
+      model_->setData(model_->index(index.row(), model_->fieldIndex("sticky")), 1);
+    } else {
+      model_->setData(model_->index(index.row(), model_->fieldIndex("sticky")), 0);
+    }
+    setCurrentIndex(curIndex);
+    event->ignore();
+    return;
+  } else if (index.column() == model_->fieldIndex("read")) {
+    if (model_->index(index.row(), model_->fieldIndex("read")).data(Qt::EditRole).toInt() == 0) {
+      emit signalSetItemRead(index, 1);
+    } else {
+      emit signalSetItemRead(index, 0);
+    }
+    event->ignore();
+    return;
+  }
+  QTreeView::mousePressEvent(event);
+}
+
+/*virtual*/ void NewsView::mouseMoveEvent(QMouseEvent *event)
+{
+}
+
+/*virtual*/ void NewsView::mouseDoubleClickEvent(QMouseEvent *event)
+{
+  emit signalDoubleClicked(indexAt(event->pos()));
+}
