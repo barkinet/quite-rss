7aee14b37c7b7f6051190a9cd96ebd9e99665b84
Aleksey Khokhryakov <aleksey.khokhryakov@gmail.com>
2012-11-23 11:03:54 +0600

    added: ? ??????? ? ????????? ???????????? ????? ??????? mercurial'a   ??? ????????? ?????? ?????????? ????????????? qmake

---
+++ src/updateappdialog.h
@@ -0,0 +1,44 @@
+#ifndef UPDATEAPPDIALOG_H
+#define UPDATEAPPDIALOG_H
+
+#include <QtGui>
+#include <QNetworkReply>
+#include <QWebPage>
+#include <QWebFrame>
+
+class UpdateAppDialog : public QDialog
+{
+  Q_OBJECT
+private:
+  QString lang_;
+  QSettings *settings_;
+  bool showDialog_;
+
+  QNetworkAccessManager manager_;
+  QNetworkReply *reply_;
+  QNetworkReply *historyReply_;
+
+  QLabel *infoLabel;
+  QTextBrowser *history_;
+
+  QPushButton *updateButton_;
+
+  QWebPage page;
+
+public:
+  explicit UpdateAppDialog(const QString &lang, QSettings *settings,
+                           QWidget *parent, bool show = true);
+
+private slots:
+  void closeDialog();
+  void finishUpdatesChecking();
+  void slotFinishHistoryReply();
+  void updaterRun();
+  void renderStatistics();
+
+signals:
+  void signalNewVersion(bool newVersion);
+
+};
+
+#endif // UPDATEAPPDIALOG_H
